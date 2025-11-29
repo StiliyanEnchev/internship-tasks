26fb59c1acf08d8be0b525d1c28e60bfed6479ef
StiliyanEnchev <stillg8@gmail.com>
2025-11-29 17:24:46 +0200

    add new vehicle and display all vehicled added

--- Tasks/autopark.php
+++ Tasks/autopark.php
@@ -1,85 +1,155 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 
 
-<?php 
+    <?php
+    $running = true;
 
+    abstract class Vehicle
+    {
+        public $brand;
+        public $model;
+        public $fuelConsumption;
 
-abstract class Vehicle {
-    public $brand;
-    public $model;
-    public $fuelConsumption;
-
-    public function __construct($brand, $model, $fuelConsumption) {
-       $this->brand = $brand;
-       $this->model = $model;
-       $this->fuelConsumption = $fuelConsumption;
-
+        public function __construct($brand, $model, $fuelConsumption)
+        {
+            $this->brand = $brand;
+            $this->model = $model;
+            $this->fuelConsumption = $fuelConsumption;
+        }
     }
-}
 
-class Car extends Vehicle {
-    public $seats;
+    class Car extends Vehicle
+    {
+        public $seats;
 
-    public function __construct($brand, $model, $fuelConsumption, $seats) {
-        parent::__construct($brand, $model, $fuelConsumption);        
-        $this->seats = $seats;
+        public function __construct($brand, $model, $fuelConsumption, $seats)
+        {
+            parent::__construct($brand, $model, $fuelConsumption);
+            $this->seats = $seats;
+        }
+
+        public function name()
+        {
+            return get_class($this) . ": " . $this->brand . " " . $this->model . " (Seats: " . $this->seats . ")";
+        }
     }
-}
 
-class Truck extends Vehicle{
-    public $max_capacity;
+    class Truck extends Vehicle
+    {
+        public $max_capacity;
 
-    public function __construct($brand, $model, $fuelConsumption, $max_capacity) {
-        parent::__construct($brand, $model, $fuelConsumption);        
-        $this->max_capacity = $max_capacity;
+        public function __construct($brand, $model, $fuelConsumption, $max_capacity)
+        {
+            parent::__construct($brand, $model, $fuelConsumption);
+            $this->max_capacity = $max_capacity;
+        }
+
+        public function name()
+        {
+            return get_class($this) . ": " . $this->brand . " " . $this->model . " (Capacity: " . $this->max_capacity . "kg)";
+        }
     }
-}
 
-class FleetManager {
-    private $vehicles = [];
+    class FleetManager
+    {
+        public $vehicles = [];
+
+        public function add_new_vehicle($type)
+        {
+
+            echo "Enter brand: ";
+            $brand = trim(fgets(STDIN));
+
+            echo "Enter model: ";
+            $model = trim(fgets(STDIN));
+
+            echo "Enter fuel consumption: ";
+            $fuelConsumption = trim(fgets(STDIN));
+
+            if ($type == "car") {
+                echo "Enter seats: ";
+                $seats = trim(fgets(STDIN));
+
+                $vehicle = new Car($brand, $model, $fuelConsumption, $seats);
+                $this->vehicles[] = $vehicle;
+            } elseif ($type == "truck") {
+                echo "Enter max capacity: ";
+                $max_capacity = trim(fgets(STDIN));
+
+                $vehicle = new Truck($brand, $model, $fuelConsumption, $max_capacity);
+                $this->vehicles[] = $vehicle;
+            }
+
+            echo "Vehicle added!\n";
+        }
 
 
-    public function run() {
-        $running = true;
-
-        while ($running) {
-            echo "1. Add new vehicle\n";
-            echo "2. Show all vehicles\n";
-            echo "3. Calculate average fuel consumption\n";
-            echo "4. Filter by vehicle type\n";
-            echo "5. Exit\n";
-            echo "Choose option: "; 
-                
-            $choice = trim(fgets(STDIN));
-
-            switch ($choice) {
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    break;
-                case 5:
-                    $running = false;
-                    echo "Program ended by the user";
-                    break;
+        function display_all_vehicles()
+        {
+            foreach ($this->vehicles as $vehicle) {
+                echo $vehicle->name() . "\n";
             }
         }
-    }  
-}
 
-$fleet = new FleetManager();
-$fleet->run()
+        function fuel_consumption()
+        {
+            $total_consumption = 0;
+            foreach ($this->vehicles as $vehicle) {
+                $total_consumption += $vehicle->fuel_consumption;
+            }
+
+            echo $total_consumption;
+        }
+    }
+
+    $fleet = new FleetManager();
 
 
 
+    while ($running) {
+        echo "1. Add new vehicle\n";
+        echo "2. Show all vehicles\n";
+        echo "3. Calculate average fuel consumption\n";
+        echo "4. Filter by vehicle type\n";
+        echo "5. Exit\n";
+        echo "Choose option: ";
 
-?>
+        $choice = trim(fgets(STDIN));
+
+        switch ($choice) {
+            case 1:
+                echo "Type 1 for Car or 2 for Truck: \n";
+                $type = trim(fgets(STDIN));
+
+                if ($type != 1 && $type != 2) {
+                    echo "Type not allowed\n";
+                    break;
+                }
+
+                $typeName = ($type == 1) ? "car" : "truck";
+                $fleet->add_new_vehicle($typeName);
+                break;
+
+            case 2:
+                $fleet->display_all_vehicles();
+                break;
+            case 3:
+                $fleet->fuel_consumption();
+                break;
+            case 4:
+                break;
+            case 5:
+                $running = false;
+                echo "Program ended by the user";
+                break;
+        }
+    }
+
+    ?>
 
 </head>
+
 <body>
-    
