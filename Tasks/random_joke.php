2bc272fdefcfdeb5302e63423690a7e2c9f8a041
StiliyanEnchev <stillg8@gmail.com>
2025-12-01 15:02:56 +0200

    random joke task completed

--- Tasks/random_joke.php
+++ Tasks/random_joke.php
@@ -1,5 +1,27 @@
 <?php
 
+$running = true;
+$url = "https://official-joke-api.appspot.com/jokes/random";
 
+while ($running) {
+    global $url;
+    
+    $response = file_get_contents($url);
+    $data = json_decode($response, true);
 
-?>
+    $setup = $data['setup'];
+    $punchline = $data['punchline'];
+
+    echo "Шега:\n";
+    echo "Setup: $setup\n";
+    echo "Punchline: $punchline\n";
+    echo "Искаш ли нова шега? (y/n): \n";
+
+    $choice = trim(fgets(STDIN));
+
+    if ($choice == "y") {
+        continue;
+    } elseif ($choice == 'n') {
+        $running = false;
+    }
+}
